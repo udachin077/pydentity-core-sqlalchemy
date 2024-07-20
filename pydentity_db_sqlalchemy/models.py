de22f09246c43d9f2ee1d5ceb0cd1d69b84f2109
udachin077 <pypi.udachin@yandex.ru>
2024-07-21 00:36:12 +0300

    Update models and stores

--- pydentity_db_sqlalchemy/models.py
+++ pydentity_db_sqlalchemy/models.py
@@ -1,21 +1,31 @@
 import datetime
-import uuid
 from typing import Optional, TYPE_CHECKING, List
+from uuid import uuid4
 
 import sqlalchemy as sa
-from sqlalchemy.ext.asyncio import AsyncAttrs
-from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
-
 from pydentity.types import TKey, GUID
+from sqlalchemy.ext.asyncio import AsyncAttrs
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship, declared_attr
+
+__all__ = (
+    'Model',
+    'IdentityUser',
+    'IdentityRole',
+    'IdentityUserRole',
+    'IdentityUserClaim',
+    'IdentityUserLogin',
+    'IdentityUserToken',
+    'IdentityRoleClaim',
+)
 
 
-class DbModel(AsyncAttrs, DeclarativeBase):
-    pass
+class Model(AsyncAttrs, DeclarativeBase):
+    __table_args__ = {'extend_existing': True}
 
 
-class IdentityUser(DbModel):
-    __tablename__ = "pydentity_users"
-    __table_args__ = {"extend_existing": True}
+class AbstractIdentityUser(Model):
+    __tablename__ = 'pydentity_identity_users'
+    __abstract__ = True
 
     if TYPE_CHECKING:
         access_failed_count: int
@@ -35,10 +45,10 @@ class IdentityUser(DbModel):
         username: Optional[str]
     else:
         access_failed_count: Mapped[int] = mapped_column(sa.Integer, default=0)
-        concurrency_stamp: Mapped[Optional[uuid.UUID]] = mapped_column(sa.String(36), nullable=True)
+        concurrency_stamp: Mapped[Optional[GUID]] = mapped_column(sa.UUID, nullable=True)
         email: Mapped[Optional[str]] = mapped_column(sa.String(256), nullable=True)
         email_confirmed: Mapped[bool] = mapped_column(sa.Boolean, default=False)
-        id: Mapped[uuid.UUID] = mapped_column(sa.String(36), primary_key=True)
+        id: Mapped[TKey] = mapped_column(sa.UUID, primary_key=True)
         lockout_enabled: Mapped[bool] = mapped_column(sa.Boolean, default=True)
         lockout_end: Mapped[Optional[datetime.datetime]] = mapped_column(sa.TIMESTAMP(timezone=True), nullable=True)
         normalized_email: Mapped[Optional[str]] = mapped_column(sa.String(256), nullable=True, unique=True)
@@ -46,38 +56,14 @@ class IdentityUser(DbModel):
         password_hash: Mapped[Optional[str]] = mapped_column(sa.Text, nullable=True)
         phone_number: Mapped[Optional[str]] = mapped_column(sa.String(256), nullable=True)
         phone_number_confirmed: Mapped[bool] = mapped_column(sa.Boolean, default=False)
-        security_stamp: Mapped[Optional[uuid.UUID]] = mapped_column(sa.String(36), nullable=True)
+        security_stamp: Mapped[Optional[GUID]] = mapped_column(sa.UUID, nullable=True)
         two_factor_enabled: Mapped[bool] = mapped_column(sa.Boolean, default=False)
         username: Mapped[Optional[str]] = mapped_column(sa.String(256), nullable=True)
 
-        roles: Mapped[List["IdentityRole"]] = relationship(
-            "IdentityRole", secondary="pydentity_user_roles", back_populates="users"
-        )
-        claims: Mapped[List["IdentityUserClaim"]] = relationship("IdentityUserClaim", back_populates="user")
-        logins: Mapped[List["IdentityUserLogin"]] = relationship("IdentityUserLogin", back_populates="user")
-        tokens: Mapped[List["IdentityUserToken"]] = relationship("IdentityUserToken", back_populates="user")
 
-        def __init__(
-                self,
-                username: str = None,
-                email: str = None,
-                **kwargs
-        ):
-            super().__init__(
-                id=str(uuid.uuid4()),
-                security_stamp=str(uuid.uuid4()),
-                username=username,
-                email=email,
-                **kwargs
-            )
-
-        def __str__(self):
-            return self.username or self.email or self.id
-
-
-class IdentityRole(DbModel):
-    __tablename__ = "pydentity_roles"
-    __table_args__ = {"extend_existing": True}
+class AbstractIdentityRole(Model):
+    __tablename__ = 'pydentity_identity_roles'
+    __abstract__ = True
 
     if TYPE_CHECKING:
         concurrency_stamp: Optional[GUID]
@@ -85,64 +71,53 @@ class IdentityRole(DbModel):
         name: Optional[str]
         normalized_name: Optional[str]
     else:
-        concurrency_stamp: Mapped[Optional[uuid.UUID]] = mapped_column(sa.String(36), nullable=True)
-        id: Mapped[uuid.UUID] = mapped_column(sa.String(36), primary_key=True)
+        concurrency_stamp: Mapped[Optional[GUID]] = mapped_column(sa.UUID, nullable=True)
+        id: Mapped[TKey] = mapped_column(sa.UUID, primary_key=True)
         name: Mapped[Optional[str]] = mapped_column(sa.String(256), nullable=True)
         normalized_name: Mapped[Optional[str]] = mapped_column(sa.String(256), nullable=True, unique=True)
 
-        users: Mapped[List["IdentityUser"]] = relationship(
-            "IdentityUser", secondary="pydentity_user_roles", back_populates="roles"
-        )
 
-        def __init__(self, name: str = None, **kwargs):
-            super().__init__(
-                id=str(uuid.uuid4()),
-                name=name,
-                **kwargs
-            )
-
-        def __str__(self):
-            return self.name or self.id
-
-
-class IdentityUserRole(DbModel):
-    __tablename__ = "pydentity_user_roles"
-    __table_args__ = {"extend_existing": True}
+class AbstractIdentityUserRole(Model):
+    __tablename__ = 'pydentity_identity_user_roles'
+    __abstract__ = True
 
     if TYPE_CHECKING:
         user_id: TKey
         role_id: TKey
     else:
-        user_id = sa.Column(sa.String(36), sa.ForeignKey(
-            "pydentity_users.id", ondelete="CASCADE"), primary_key=True
-                            )
-        role_id = sa.Column(sa.String(36), sa.ForeignKey(
-            "pydentity_roles.id", ondelete="CASCADE"), primary_key=True
-                            )
+        user_id: Mapped[TKey] = mapped_column(
+            sa.UUID,
+            sa.ForeignKey('pydentity_identity_users.id', ondelete='CASCADE'),
+            primary_key=True
+        )
+        role_id: Mapped[TKey] = mapped_column(
+            sa.UUID,
+            sa.ForeignKey('pydentity_identity_roles.id', ondelete='CASCADE'),
+            primary_key=True
+        )
 
 
-class IdentityUserClaim(DbModel):
-    __tablename__ = "pydentity_user_claims"
-    __table_args__ = {"extend_existing": True}
+class AbstractIdentityUserClaim(Model):
+    __tablename__ = 'pydentity_identity_user_claims'
+    __abstract__ = True
 
     if TYPE_CHECKING:
         claim_type: Optional[str]
         claim_value: Optional[str]
         user_id: TKey
     else:
-        claim_type: Mapped[Optional[str]] = mapped_column(sa.String(455), primary_key=True)
-        claim_value: Mapped[Optional[str]] = mapped_column(sa.Text, nullable=True)
-        user_id: Mapped[uuid.UUID] = mapped_column(
-            sa.ForeignKey("pydentity_users.id", ondelete="CASCADE"),
+        user_id: Mapped[TKey] = mapped_column(
+            sa.UUID,
+            sa.ForeignKey('pydentity_identity_users.id', ondelete='CASCADE'),
             primary_key=True
         )
+        claim_type: Mapped[str] = mapped_column(sa.String(455), primary_key=True)
+        claim_value: Mapped[Optional[str]] = mapped_column(sa.Text, nullable=True)
 
-        user: Mapped["IdentityUser"] = relationship("IdentityUser", back_populates="claims")
 
-
-class IdentityUserLogin(DbModel):
-    __tablename__ = "pydentity_user_logins"
-    __table_args__ = {"extend_existing": True}
+class AbstractIdentityUserLogin(Model):
+    __tablename__ = 'pydentity_identity_user_logins'
+    __abstract__ = True
 
     if TYPE_CHECKING:
         login_provider: str
@@ -150,20 +125,19 @@ class IdentityUserLogin(DbModel):
         provider_display_name: Optional[str]
         user_id: TKey
     else:
+        user_id: Mapped[TKey] = mapped_column(
+            sa.UUID,
+            sa.ForeignKey('pydentity_identity_users.id', ondelete='CASCADE'),
+            primary_key=True
+        )
         login_provider: Mapped[str] = mapped_column(sa.String(256), primary_key=True)
         provider_key: Mapped[str] = mapped_column(sa.String(256))
         provider_display_name: Mapped[Optional[str]] = mapped_column(sa.Text, nullable=True)
-        user_id: Mapped[uuid.UUID] = mapped_column(sa.ForeignKey(
-            "pydentity_users.id", ondelete="CASCADE"),
-            primary_key=True
-        )
-
-        user: Mapped["IdentityUser"] = relationship("IdentityUser", back_populates="logins")
 
 
-class IdentityUserToken(DbModel):
-    __tablename__ = "pydentity_user_tokens"
-    __table_args__ = {"extend_existing": True}
+class AbstractIdentityUserToken(Model):
+    __tablename__ = 'pydentity_identity_user_tokens'
+    __abstract__ = True
 
     if TYPE_CHECKING:
         login_provider: str
@@ -171,12 +145,134 @@ class IdentityUserToken(DbModel):
         value: Optional[str]
         user_id: TKey
     else:
+        user_id: Mapped[TKey] = mapped_column(
+            sa.UUID,
+            sa.ForeignKey('pydentity_identity_users.id', ondelete='CASCADE'),
+            primary_key=True
+        )
         login_provider: Mapped[str] = mapped_column(sa.String(256), primary_key=True)
         name: Mapped[str] = mapped_column(sa.String(256))
         value: Mapped[Optional[str]] = mapped_column(sa.Text, nullable=True)
-        user_id: Mapped[uuid.UUID] = mapped_column(
-            sa.ForeignKey("pydentity_users.id", ondelete="CASCADE"),
+
+
+class AbstractIdentityRoleClaim(Model):
+    __tablename__ = 'pydentity_identity_role_claims'
+    __abstract__ = True
+
+    if TYPE_CHECKING:
+        claim_type: Optional[str]
+        claim_value: Optional[str]
+        role_id: TKey
+    else:
+        role_id: Mapped[TKey] = mapped_column(
+            sa.UUID,
+            sa.ForeignKey('pydentity_identity_roles.id', ondelete='CASCADE'),
             primary_key=True
         )
+        claim_type: Mapped[str] = mapped_column(sa.String(455), primary_key=True)
+        claim_value: Mapped[Optional[str]] = mapped_column(sa.Text, nullable=True)
 
-        user: Mapped["IdentityUser"] = relationship("IdentityUser", back_populates="tokens")
+
+class IdentityUser(AbstractIdentityUser):
+    @declared_attr
+    def roles(cls) -> Mapped[List['IdentityRole']]:
+        return relationship(
+            'IdentityRole',
+            back_populates='users',
+            secondary='pydentity_identity_user_roles'
+        )
+
+    @declared_attr
+    def claims(cls) -> Mapped[List['IdentityUserClaim']]:
+        return relationship('IdentityUserClaim', back_populates='user')
+
+    @declared_attr
+    def logins(cls) -> Mapped[List['IdentityUserClaim']]:
+        return relationship('IdentityUserLogin', back_populates='user')
+
+    @declared_attr
+    def tokens(cls) -> Mapped[List['IdentityUserClaim']]:
+        return relationship('IdentityUserToken', back_populates='user')
+
+    def __init__(
+            self,
+            email: str,
+            username: Optional[str] = None,
+            **kwargs
+    ):
+        super().__init__(
+            id=uuid4(),
+            email=email,
+            username=username,
+            security_stamp=uuid4(),
+            **kwargs
+        )
+
+    def __str__(self):
+        return self.username or self.email or self.id
+
+
+class IdentityRole(AbstractIdentityRole):
+    @declared_attr
+    def users(cls) -> Mapped[List['IdentityUser']]:
+        return relationship(
+            'IdentityUser',
+            back_populates='roles',
+            secondary='pydentity_identity_user_roles'
+        )
+
+    @declared_attr
+    def claims(cls) -> Mapped[List['IdentityRoleClaim']]:
+        return relationship('IdentityRoleClaim', back_populates='role')
+
+    def __init__(self, name: str, **kwargs):
+        super().__init__(
+            id=uuid4(),
+            name=name,
+            **kwargs
+        )
+
+    def __str__(self):
+        return self.name or self.id
+
+
+class IdentityUserRole(AbstractIdentityUserRole):
+    pass
+
+
+class IdentityUserClaim(AbstractIdentityUserClaim):
+    @declared_attr
+    def user(self) -> Mapped['IdentityUser']:
+        return relationship('IdentityUser', back_populates='claims')
+
+
+class IdentityUserLogin(AbstractIdentityUserLogin):
+    @declared_attr
+    def user(self) -> Mapped['IdentityUser']:
+        return relationship('IdentityUser', back_populates='logins')
+
+
+class IdentityUserToken(AbstractIdentityUserToken):
+    @declared_attr
+    def user(self) -> Mapped['IdentityUser']:
+        return relationship('IdentityUser', back_populates='tokens')
+
+
+class IdentityRoleClaim(AbstractIdentityRoleClaim):
+    @declared_attr
+    def role(self) -> Mapped['IdentityRole']:
+        return relationship('IdentityRole', back_populates='claims')
+
+
+if __name__ == '__main__':
+    from sqlalchemy.ext.asyncio import create_async_engine
+    import asyncio
+
+
+    async def main():
+        async_engine = create_async_engine("sqlite+aiosqlite:///./test.db")
+        async with async_engine.begin() as conn:
+            await conn.run_sync(Model.metadata.create_all)
+
+
+    asyncio.run(main())
--- pydentity_db_sqlalchemy/stores/role_store.py
+++ pydentity_db_sqlalchemy/stores/role_store.py
@@ -1,18 +1,20 @@
 from typing import Type, Generic, Optional
-from uuid import uuid4
+from uuid import uuid4, UUID
 
-from pydentity.abc.stores import IRoleStore
+from pydentity.abc.stores import IRoleStore, IRoleClaimStore
 from pydentity.exc import ArgumentNoneException
 from pydentity.identity_result import IdentityResult
-from pydentity.types import TRole
-from sqlalchemy import select
+from pydentity.security.claims import Claim
+from pydentity.types import TRole, TRoleClaim
+from sqlalchemy import select, and_, delete
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from pydentity_db_sqlalchemy.models import IdentityRole
+from pydentity_db_sqlalchemy.models import IdentityRole, IdentityRoleClaim
 
 
-class RoleStore(IRoleStore[TRole], Generic[TRole]):
+class RoleStore(IRoleClaimStore[TRole], IRoleStore[TRole], Generic[TRole]):
     role_model: Type[TRole] = IdentityRole
+    role_claim_model: Type[TRoleClaim] = IdentityRoleClaim
 
     def __init__(self, session: AsyncSession):
         self.session = session
@@ -27,8 +29,7 @@ class RoleStore(IRoleStore[TRole], Generic[TRole]):
         await self.session.refresh(role)
 
     async def all(self) -> list[TRole]:
-        statement = select(self.role_model)
-        return list((await self.session.scalars(statement)).all())
+        return list((await self.session.scalars(select(self.role_model))).all())
 
     async def create(self, role: TRole) -> IdentityResult:
         if role is None:
@@ -43,7 +44,7 @@ class RoleStore(IRoleStore[TRole], Generic[TRole]):
         if role is None:
             raise ArgumentNoneException("role")
 
-        role.concurrency_stamp = str(uuid4())
+        role.concurrency_stamp = uuid4()
         await self.save_changes()
         await self.refresh(role)
         return IdentityResult.success()
@@ -60,15 +61,18 @@ class RoleStore(IRoleStore[TRole], Generic[TRole]):
         if role_id is None:
             raise ArgumentNoneException("role_id")
 
+        role_id = UUID(role_id) if isinstance(role_id, str) else role_id
         statement = select(self.role_model).where(self.role_model.id == role_id)
-        return await self._find_role(statement)
+        result = await self.session.execute(statement)
+        return result.scalar_one_or_none()
 
     async def find_by_name(self, normalized_name: str) -> Optional[TRole]:
         if normalized_name is None:
             raise ArgumentNoneException("normalized_name")
 
         statement = select(self.role_model).where(self.role_model.normalized_name == normalized_name)  # type: ignore
-        return await self._find_role(statement)
+        result = await self.session.execute(statement)
+        return result.scalar_one_or_none()
 
     async def get_role_id(self, role: TRole) -> str:
         if role is None:
@@ -100,5 +104,50 @@ class RoleStore(IRoleStore[TRole], Generic[TRole]):
 
         role.normalized_name = normalized_name
 
-    async def _find_role(self, statement) -> Optional[TRole]:
-        return (await self.session.execute(statement)).scalar_one_or_none()
+    async def add_claim(self, role: TRole, claim: Claim) -> None:
+        if role is None:
+            raise ArgumentNoneException("role")
+        if claim is None:
+            raise ArgumentNoneException("claim")
+
+        self.session.add(self._create_role_claim(role, claim))
+        await self.save_changes()
+
+    async def remove_claim(self, role: TRole, claim: Claim) -> None:
+        if role is None:
+            raise ArgumentNoneException("role")
+        if claim is None:
+            raise ArgumentNoneException("claim")
+
+        statement = delete(self.role_claim_model).where(
+            and_(
+                self.role_claim_model.role_id == role.id,
+                self.role_claim_model.claim_type == claim.type,
+                self.role_claim_model.claim_value == claim.value
+            )
+        )
+        await self.session.execute(statement)
+
+    async def get_claims(self, role: TRole) -> list[Claim]:
+        if role is None:
+            raise ArgumentNoneException("role")
+
+        statement = (
+            select(self.role_claim_model)
+            .where(self.role_claim_model.role_id == user.id)  # type: ignore
+        )
+        role_claims = (await self.session.scalars(statement)).all()
+        return [self._create_claim(uc) for uc in role_claims]
+
+    def _create_claim(self, model: TRoleClaim) -> Claim:  # noqa
+        return Claim(
+            claim_type=model.claim_type,
+            claim_value=model.claim_value,
+        )
+
+    def _create_role_claim(self, role: TRole, claim: Claim) -> TRoleClaim:
+        return self.role_claim_model(
+            role_id=role.id,
+            claim_type=claim.type,
+            claim_value=claim.value
+        )
