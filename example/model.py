2449a9522d73090e60a93ef984e1da4bfc40ab61
udachin077 <pypi.udachin@yandex.ru>
2024-07-22 00:03:39 +0300

    add example

---
+++ example/model.py
@@ -0,0 +1,46 @@
+"""
+    CREATE TABLE pydentity_users (
+        first_name VARCHAR(256),
+        last_name VARCHAR(256),
+        ...
+    )
+
+    CREATE TABLE orders (
+        id INTEGER NOT NULL,
+        number VARCHAR(100) NOT NULL,
+        user_id VARCHAR(450),
+        PRIMARY KEY (id)
+)
+"""
+import asyncio
+
+import sqlalchemy as sa
+from sqlalchemy.ext.asyncio import create_async_engine
+from sqlalchemy.orm import mapped_column, Mapped
+
+from pydentity_db_sqlalchemy.models.abstract import AbstractIdentityUser, Model
+
+
+class CustomUser(AbstractIdentityUser):
+    first_name: Mapped[str] = mapped_column(sa.String(256))
+    last_name: Mapped[str] = mapped_column(sa.String(256))
+
+
+class Order(Model):
+    __tablename__ = 'orders'
+
+    id: Mapped[int] = mapped_column(sa.Integer, primary_key=True, autoincrement=True)
+    number: Mapped[str] = mapped_column(sa.String(100))
+    user_id = mapped_column(
+        sa.ForeignKey('pydentity_users.id', ondelete='CASCADE')
+    )
+
+
+async def main():
+    async_engine = create_async_engine("sqlite+aiosqlite://", echo=True)
+    async with async_engine.begin() as conn:
+        await conn.run_sync(Model.metadata.create_all)
+
+
+if __name__ == '__main__':
+    asyncio.run(main())
